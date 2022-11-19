3d685116331be65da22e7e547da3d8a385d28440
Rintu <Rintu@VINDUJA>
2022-11-19 12:18:43 +0530

    Add project files.

---
+++ Test3_Vinay_SalaryCalculations_EmpLibrary/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test3_Vinay_SalaryCalculations_EmpLibrary;
+
+Console.WriteLine("Hello, World!");
+
+Salary objectSalary = new Salary();
+objectSalary.CalculateSalary();
+
